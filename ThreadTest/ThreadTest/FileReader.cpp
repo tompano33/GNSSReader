e8efa66636f6d9bae3ee43b8d3d6dd574cc74769
William J Liddy <wjl36@case.edu>
2015-06-17 13:36:04 -0400

    Readfile buffer system implemented, now trying to multithread

--- ThreadTest/ThreadTest/FileReader.cpp
+++ ThreadTest/ThreadTest/FileReader.cpp
@@ -3,6 +3,81 @@
 #include <iostream>
 #include "FileReader.h"
 
+//try std::vector?
+class TestBuffer{
+	static const int BUFFERSIZE = 7;
+	//The intermediate buffer between the raw file on disk and the decoder
+	char* ibuf;
+	//Pointer to where we may write new bytes to the buffer if there is space.
+	int bufPtr;
+	//Pointer to where the start of the oldest data is in the buffer
+	int oldPtr;
+	//return buffer for when we want to read bytes.
+	char* returnbuf;
+	//count of all the bytes that are stored
+	volatile int numBytesStored;
+
+public:
+	TestBuffer::TestBuffer(){
+		ibuf = new char[BUFFERSIZE];
+		bufPtr = 0;
+		oldPtr = 0;
+		numBytesStored = 0;
+	}
+
+	bool write(char* bytes, int count)
+	{
+		//Not enough room to put bytes!
+		if((BUFFERSIZE-numBytesStored) < count){
+			return false;
+		} else
+		{
+			numBytesStored += count;
+
+			if(count < (BUFFERSIZE - bufPtr) ) 
+			{
+				memcpy(ibuf+bufPtr, bytes, count);
+				bufPtr += count;
+			}
+			else
+			{
+				int spaceLeftAtEnd = (BUFFERSIZE - bufPtr);
+				memcpy(ibuf+bufPtr,bytes,spaceLeftAtEnd);
+				memcpy(ibuf,bytes+spaceLeftAtEnd,count-spaceLeftAtEnd);
+				bufPtr = count-spaceLeftAtEnd;
+			}
+			return true;
+		}
+	}
+
+	bool read(int size, char* buf){
+		if(size > numBytesStored)
+		{
+			return false;
+		} else
+		{
+			numBytesStored -=size;
+			if(size < (BUFFERSIZE - oldPtr))
+			{
+				memcpy(buf,ibuf+oldPtr,size);
+				oldPtr+= size;
+			} else 
+			{
+				int spaceLeftAtEnd = (BUFFERSIZE - oldPtr);
+				memcpy(buf,ibuf+oldPtr,spaceLeftAtEnd);
+				memcpy(buf+spaceLeftAtEnd,ibuf,size-spaceLeftAtEnd);
+				oldPtr=size-spaceLeftAtEnd;
+			}
+			return true;
+		}
+	}
+
+	~TestBuffer(){
+		delete [] ibuf;
+	}
+};
+
+
 
 FileReader::FileReader(void)
 {
@@ -11,22 +86,26 @@ FileReader::FileReader(void)
 
 FileReader::~FileReader(void)
 {
-}
 
-  
+}
 
 
 int main(void)
 {
 
-	HANDLE hFile;
-	HANDLE hAppend;
+	HANDLE sdrFile;
 	DWORD dwBytesRead, dwBytesWritten, dwPos;
-	LPCWSTR fname = L"c:\\test\\1.txt";
-	LPCWSTR fname2 = L"c:\\test\\2.txt";
-	char buff[4096];
+	dwBytesRead = 0;
+	const unsigned long fileSize = 36;
+	const unsigned long readBufferSize = 3;
+	unsigned long bufferPointer;
+	int bytesRead = 0;
+	LPCWSTR fname = L"C:\\Users\\ANTadmin\\Desktop\\GNSSReader\\Tests\\ThreadTest.txt";
+	char buff[readBufferSize];
 	// Open the existing file.
-	hFile = CreateFile(fname, // open testfile.txt
+
+	sdrFile = CreateFile(
+	fname, //filename.
 	GENERIC_READ, // open for reading
 	0, // do not share
 	NULL, // default security
@@ -34,85 +113,39 @@ int main(void)
 	FILE_ATTRIBUTE_NORMAL, // normal file
 	NULL); // no attribute template
 
-	if(hFile == INVALID_HANDLE_VALUE)
+	if(sdrFile == INVALID_HANDLE_VALUE)
+	{
 		printf("Could not open %S.\n", fname);
-	else
-		printf("%S opened successfully.\n", fname);
-// Open the existing file, or if the file does not exist,
-// create a new file.
-
-	hAppend = CreateFile(fname2, // open mytestfiletwo.txt
-
-	GENERIC_WRITE, // open for writing
-	0, // do not share
-	NULL, // default security
-	OPEN_ALWAYS, // open or create
-	FILE_ATTRIBUTE_NORMAL, // normal file
-	NULL); // no attribute template
-
- 
-
-	if(hAppend == INVALID_HANDLE_VALUE)
-		printf("Could not open %S.\n", fname2);
-
-	else
-	{
-
-	printf("%S opened/created successfully.\n", fname2);
-
-	printf("\nAppending %S\'s content to %S\'s content\n", fname, fname2);
-
-	printf("Check the %S content \n\n", fname2);
-
+		CloseHandle(sdrFile);
+		std::cin.get();
+		return 0;
 	}
 
- 
+	TestBuffer* tb = new TestBuffer();
+	char* foobuf = new char[10];
+	while(bytesRead < fileSize){
+		bool readFile = ReadFile(sdrFile, buff, readBufferSize, &dwBytesRead, NULL);
+		if(readFile){
+			while(!tb->write(buff,readBufferSize)){
+				tb->read(2,foobuf);
+				bytesRead += 2;
+				std::cout<< foobuf[0];
+				std::cout<< foobuf[1];
+				std::cin.get();
+			}
 
-	// Append the first file to the end of the second file.
+			std::cout << "Read All! " << bytesRead << " / " << fileSize << std::endl;
+		} else {
+			while(!tb->write(buff,dwBytesRead));
+			bytesRead += dwBytesRead;
+			std::cout << "Read Some " << bytesRead << " / " << fileSize << std::endl;
+		}
+	} 
 
-	// Lock the second file to prevent another process from
+	//close file
+	if(CloseHandle(sdrFile) != 0)
+		printf("\n%S file handle closed successfully!\n", fname);
 
-	// accessing it while writing to it. Unlock the file when writing is finished.
-
-	do
-
-	{
-
-	if(ReadFile(hFile, buff, 4096, &dwBytesRead, NULL))
-
-	{
-
-	dwPos = SetFilePointer(hAppend, 0, NULL, FILE_END);
-
- 
-
-	if(LockFile(hAppend, dwPos, 0, dwBytesRead, 0) != 0)
-
-	printf("Locking %S...\n", fname2);
-
-	WriteFile(hAppend, buff, dwBytesRead, &dwBytesWritten, NULL);
-
- 
-
-	if(UnlockFile(hAppend, dwPos, 0, dwBytesRead, 0) != 0)
-
-	printf("Unlocking %S...\n", fname2);
-
-	}
-
-	} while (dwBytesRead == 4096);
-
- 
-
-	// Close both files.
-
-	if(CloseHandle(hFile) != 0)
-
-	printf("\n%S file handle closed successfully!\n", fname);
-
-	if(CloseHandle(hAppend) != 0)
-
-	printf("%S file handle closed successfully!\n", fname2);
 
 	std::cin.get();
 	return 0;
