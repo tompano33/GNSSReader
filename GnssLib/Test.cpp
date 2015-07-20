8acaece415c7284d94bc13c0d8dac06827722056
William J Liddy <wjl36@case.edu>
2015-07-20 13:02:20 -0400

    Added a static library

---
+++ GnssLib/Test.cpp
@@ -0,0 +1,250 @@
+//Tests the GnssReader using files in the Test directory
+#include "GnssReader.h"
+#include <iostream>
+
+std::string pathToTests;
+
+//TODO memory leaks, but it's just a test so it's ok
+const char* pathTo(char * address)
+{
+	std::string * localPath = new std::string;
+	localPath->append(std::string (pathToTests));
+	localPath->append(std::string (address));
+	return (localPath->c_str());
+}
+
+void testSuite()
+{
+	std::cout << "Enter the path to your 'Tests' Directory:\n";
+	std::string path;
+	std::getline (std::cin, path);
+	
+	//Change this to the path to your test suite if you want to set a default directory
+	pathToTests = ("C:\\Users\\ANTadmin\\Desktop\\SDR\ STANDARD\\Tests\\");
+	if(path.size() != 0)
+	{
+		pathToTests = path; 
+	}
+
+	try
+	{
+
+
+		//alignment: Tests quantization and aligned bits. 
+		{
+			printf("Expecting: [5, -2, -16, 15]\n");
+			GNSSReader test (pathTo("alignment\\test.xml"),50L,1000L,1000000L);
+			test.makeDecStreams();
+			test.setPrintOptions(false,true);
+			test.startAsThread();
+			while(!test.isDone()){;}
+		}
+	
+
+		//altPaths: Tests quantization and aligned bits. 
+		{
+			printf("Expecting: A nice introduction \n");
+
+			const char** paths = new const char*[3];
+			paths[0] = (pathTo("altPaths\\storage\\"));
+			paths[1] = (pathTo("altPaths\\storage\\superstorage\\"));
+			paths[2] = (pathTo("altPaths\\storage\\superstorage\\extremestorage\\"));
+		
+			GNSSReader test (pathTo("altPaths\\test.xml"),6L,3L,1000000L,-1,paths,3);
+			test.makeDecStreams();
+			test.setPrintOptions(false,true);
+			test.startAsThread();
+			while(!test.isDone()){;}
+		}
+		
+		
+
+		//exception: throws error if invalid XML exists.
+		{
+			try{
+				printf("Expecting: Printed 'foostream1' \n");
+				GNSSReader test (pathTo("exception\\test.xml"),50L,1000L,1000000L);
+				test.makeDecStreams();
+				test.setPrintOptions(false,true);
+				test.startAsThread();
+				while(!test.isDone()){;}
+		
+			} catch (std::exception& e) {
+				printf(e.what());
+			}
+		}
+
+		//floatingpoint: tests reading of floating point numbers
+		{
+			printf("Expecting: [-5.168466E29 -2.4479542E-19 6.3108872E-30 0 ] and two doubles. \n");
+			GNSSReader test (pathTo("floatingPoint\\test.xml"),50L,1000L,1000000L);
+			test.makeDecStreams();
+			test.setPrintOptions(false,true);
+			test.startAsThread();
+			while(!test.isDone()){;}
+		}
+
+		//formatOne: tests IQ, IF formats
+		{
+			printf("Expecting reals of 10, complexes of -2\n");
+			GNSSReader test (pathTo("formatOne\\test.xml"),50L,1000L,1000000L,-1);
+			test.makeDecStreams();
+			test.setPrintOptions(false,true);
+			test.startAsThread();
+			while(!test.isDone()){;}
+		}
+
+		//header: tests skipping of headers and footers of various sizes.
+		{
+			printf("Expecting: [HELLO___SDR_WORLD___THISIS__A___TEST_!_]  \n");
+			GNSSReader test (pathTo("header\\test.xml"),50L,1000L,1000000L);
+			test.makeDecStreams();
+			test.setPrintOptions(false,true);
+			test.startAsThread();
+			while(!test.isDone()){
+			//std::cout << test.getIBufPercent() << "\n";
+			}
+		}
+
+		/**
+		//multistream: outputs four 1-bit streams.
+		//INVESTIGATE the mean & variance are wrong
+		{
+			GNSSReader test (pathTo("multiStream\\test.xml"),50L,1000L,1000000L,1);
+			test.makeDecStreams();
+			test.setPrintOptions(true,false);
+			test.startAsThread();
+			while(!test.isDone()){;}
+		}
+		*/
+
+		//offset-binary
+		{
+			printf("Expecting 0100 0101 0100 1100 0100 1111  -2 -3 -2 0 2 -3 -3 3 -4 -3 -4 4 -4 7\n");
+			GNSSReader test (pathTo("offsetBin\\test.xml"),50L,1000L,1000000L,-1);
+			test.makeDecStreams();
+			test.setPrintOptions(false,true);
+			test.startAsThread();
+			while(!test.isDone()){;}
+		}
+
+		//padAndShift: Tests padding and shifting.
+		{
+			printf("Expecting repeated pattern\n");
+			GNSSReader test (pathTo("padAndShift\\lshp.xml"),50L,1000L,1000000L,-1);
+			test.makeDecStreams();
+			test.setPrintOptions(false,true);
+			test.startAsThread();
+			while(!test.isDone()){;}
+		}
+
+		//padding: Tests Padding 
+		{
+			printf("Expecting repeated pattern\n");
+			GNSSReader test (pathTo("padding\\test.xml"),50L,1000L,1000000L,-1);
+			test.makeDecStreams();
+			test.setPrintOptions(false,true);
+			test.startAsThread();
+			while(!test.isDone()){;}
+		}
+
+		//SampleBits: tests samples of various sizes
+		{
+			GNSSReader test (pathTo("sampleBits\\test.xml"),50L,1000L,1000000L);
+			test.makeDecStreams();
+			test.setPrintOptions(false,true);
+			test.startAsThread();
+			while(!test.isDone()){;}
+		}
+
+		//Seven Eight: a 7 bit and 8 bit stream
+		{
+			GNSSReader test (pathTo("sevenEight\\test.xml"),50L,1000L,1000000L);
+			test.makeDecStreams();
+			test.setPrintOptions(false,true);
+			test.startAsThread();
+			while(!test.isDone()){;}
+		}
+
+		//Sign-Magnitude:
+		{
+			GNSSReader test ( pathTo("signMag\\test.xml"),50L,1000L,1000000L);
+			test.makeDecStreams();
+			test.setPrintOptions(false,true);
+			test.startAsThread();
+			while(!test.isDone()){;}
+		}
+
+		//SixtyFour
+		{
+			GNSSReader test (pathTo("sixtyFour\\test.xml"),50L,1000L,1000000L);
+			test.makeDecStreams();
+			test.setPrintOptions(false,true);
+			test.startAsThread();
+			while(!test.isDone()){;}
+			//sixtyfour does not end gracefully
+		}
+		
+	
+		//Sine as does work, but if you try to open the file again later an error is thrown. Perhaps the handle is not closed as i thought?
+		
+			/** This method does not finish! 
+			
+		{
+			//fix a chunkbuffer that is too small
+			GNSSReader test (pathTo("sine\\test.xml"),5000L,10L,1000000L,2);
+		
+			test.makeDecStreams();
+			//test.setPrintOptions(true,false);
+			test.startAsThread();
+			while(!test.isDone()){
+
+				uint64_t count = 0; 
+				test.getDecStreamArray()[0]->flushOutputStream(&count);				
+			//	std::cout << "Count is" << count << std::endl;
+			
+		//			for(int i = 0; i != count; i++)
+		//			{
+		//				std::cout << inbuf[i] << std::endl;
+		//			}
+
+
+				}
+			
+		}
+
+		*/
+
+		//SingleStream
+		{
+			GNSSReader test (pathTo("singleStream\\test.xml"),5000L,5L,1000000L,5);
+			test.makeDecStreams();
+			test.setPrintOptions(true,false);
+
+			//This won't always work on multi-blocked chunks
+			//Also add support for repeating blocks
+			test.startAtBlock(1);
+			test.startAsThread();
+			while(!test.isDone()){;}
+			//while(!test.isDone()){std::cout << test.getIBufPercent() << "\n";}
+		}
+		
+	} catch (std::exception& e) {
+		printf("\n\n ***TEST FAILED*** \n\n");
+		printf(e.what());
+	}
+}
+
+int main(int argc, char** argv)
+{
+	clock_t tStart = clock();
+
+	testSuite();
+	
+	printf("Execution Time: %.2f s\n", (double)(clock() - tStart)/CLOCKS_PER_SEC);
+
+	//_CrtDumpMemoryLeaks();
+	std::cin.get();
+	return 0;
+}
+
---
+++ GnssLib/include/ChunkBuffer.h
@@ -0,0 +1,44 @@
+/**
+ * File: ChunkBuffer.h
+ * Author: WJLIDDY
+ * Description: Points to a chunk of raw SDR data, and returns individual samples in order based on the requested encoding.
+ * TODO: While the current implementation works fine, I feel like I need to return something with Generic Types rather than making everything 64bit.
+ */
+
+#ifndef CHUNKBUFFER_H_H
+#define CHUNKBUFFER_H_H
+#include <stdint.h>
+#include <string>
+
+class ChunkBuffer{
+
+	//Pointer to buffer holding the chunk. Not to be modified.
+	char* chunkInputBuffer;	
+	//The byte our "cursor" is at in the buffer; the next byte to read
+	uint64_t bufferBytePointer; 
+	//The bit our "cursor" is at in the buffer; the next bit to read
+	uint8_t bufferBitPointer; 	
+	//The size of the buffer
+	uint64_t sizeOfBuffer;	
+	//returns true if the last value decoded was floatingpoint.
+	bool wasFloat;
+
+public:
+
+	/** Pass in the size of a buffer, and a pre-allocated buffer with the chunk already defined */
+	ChunkBuffer::ChunkBuffer(uint64_t size, char* buf); 
+
+	/** Given a number of bitsToRead and an encoding, pops a sample from the buffer, formats it to the encoding, returns it */
+	int64_t readBits(uint8_t bitsToRead, std::string  encoding);
+
+	/** Returns true if every value in the chunkBuffer has been read */
+	bool chunkFullyRead();
+
+	/** Skips over bits in the buffer */
+	void skipBits(uint8_t bitsToSkip);
+
+	/** returns true if the last sample should be read as a float */
+	bool wasSampleFloat();
+};
+
+#endif
