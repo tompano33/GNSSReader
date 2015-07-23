718665ebba349d75d560ea1fef789dab4c93f2be
William J Liddy <wjl36@case.edu>
2015-07-23 00:17:37 -0400

    Removing old ffts, builds

--- Waterfall/build/x86-build/qtenv2.bat
+++
@@ -1,5 +0,0 @@
-echo off
-echo Setting up environment for Qt usage...
-set PATH=C:\Qt\5.5\msvc2010\bin;%PATH%
-cd /D C:\Qt\5.5\msvc2010
-echo Remember to call vcvarsall.bat to complete environment setup!
--- Waterfall/fftw/3232.bat
+++
@@ -1,2 +0,0 @@
-"C:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\vcvarsall.bat" x86
-lib /machine:x86 /def:libfftw3-3.def
