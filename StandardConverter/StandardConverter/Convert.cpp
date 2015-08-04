5256cc0fa50316899dd503df88100967c81db7d2
William J Liddy <wjl36@case.edu>
2015-08-03 20:50:28 -0400

    All Critical information converted for single file, testing

--- StandardConverter/StandardConverter/Convert.cpp
+++ StandardConverter/StandardConverter/Convert.cpp
@@ -27,120 +27,13 @@ struct XMetadata {
 	const char* footerMask;
 	const char* sampleRateHz;
 	const char* nextFile;
-	vector <XStream> streams;
+	vector <XStream * > * streams;
 
 };
 
 //Current set of Metadata that has been pulled
 struct XMetadata toConvert;
 
-//temp function: ignore.
-void WriteXmlFile(const char* pszFilename)
-{
-    printf("\nWriting GNSS Metadata to xml file: %s\n", pszFilename);
-
-    ////////////////////////////////
-	//Define information about the datafile.
-
-	//UTC:    30-Dec-2014 22:38:54
-	//GPS:    1825/254334.906
-	size_t offset = 588;
-    std::string sfile = "141230-gps-4msps.bds";
-    std::string sfilemd = pszFilename;
-
-	////////////////////////////////
-	//Define the Session.
-	Session sess("0");
-	sess.Scenario("Example 2");
-	sess.Campaign("GNSS Metadata API Testing");
-	sess.Contact("J. Doe");
-	sess.Position( Position(47.76471300, -122.15612900, 25.610));
-	sess.AddComment("This is an example metadata specification with two interleaved streams.");
-
-	////////////////////////////////
-	//Define the System, Sources, and cluster.
-	System sys("A2300-1");
-	sys.BaseFrequency( Frequency( 4, Frequency::MHz));
-	sys.Equipment("ASR-2300");
-	sys.AddComment( "ASR-2300 configured with standard firmware and FPGA id=1, version=1.18.");
-
-	Cluster clstr("Antenna");
-
-	Source src1( Source::Patch, Source::RHCP, "L1 C/A");
-	src1.IdCluster("Antenna");
-
-	Source src2( Source::Patch, Source::RHCP, "L2 C");
-	src2.IdCluster("Antenna");
-
-	sys.AddSource( src1);
-	sys.AddSource( src2);
-	sys.AddCluster(clstr);
-
-	////////////////////////////////
-	//Define Band 1 and L1 C/A Stream.
-	Band ch1("L1External");
-	ch1.CenterFrequency(Frequency( 1575.42, Frequency::MHz));
-	ch1.TranslatedFrequency(Frequency( 38400, Frequency::Hz));
-
-	Stream sm1("L1ca");
-	sm1.RateFactor(1);
-	sm1.Quantization(8);
-	sm1.Packedbits(16);
-	sm1.Encoding("INT8");
-	sm1.Format(Stream::IQ);
-	sm1.Bands().push_back( ch1);
-
-	////////////////////////////////
-	//Define Band 2 and L2 C Stream.
-	Band ch2("L2cExternal");
-	ch2.CenterFrequency(Frequency( 1227.6, Frequency::MHz));
-	ch2.TranslatedFrequency(Frequency( 38400, Frequency::Hz));
-
-	//Stream sm2 will be added to the global metadata
-	//and as a reference to the lump.
-	Stream sm2("L2C");
-	sm2.RateFactor(1);
-	sm2.Quantization(8);
-	sm2.Packedbits(16);
-	sm2.Encoding("INT8");
-	sm2.Format(Stream::IQ);
-	sm2.Bands().push_back(ch2);
-
-	////////////////////////////////
-	//Define the lane
-	Lump lump;
-	lump.Streams().push_back( sm1);
-	lump.Streams().push_back( sm2.ToReference<Stream>());	
-
-
-	Chunk chunk;
-	chunk.SizeWord(4);
-	chunk.CountWords(1);
-	chunk.Lumps().push_back(lump);
-
-	Block blk(256);
-	blk.Chunks().push_back(chunk);
-	
-	Lane lane("GPS SPS Data");
-	lane.Sessions().push_back( sess);
-	lane.Blocks().push_back(blk);
-	lane.AddBandSource(ch1, src1);
-	lane.AddBandSource(ch2, src2);
-	lane.Systems().push_back( sys.ToReference<System>());
-
-	////////////////////////////////
-	//Define the file
-	File df;
-	df.Url( sfile);
-	df.Lane( lane, true);
-
-	//Note: we aren't specifying copyright or owner.
-
-
-	////////////////////////////////
-	//Assemble the Metadata object and write XML 
-
-}
 
 //Given an old XML document, pulls metadata to be put in toConvert.
 bool pullXMetadata(XMLDocument*);
@@ -222,55 +115,44 @@ void main()
 
 		//then, write a block to the lane.
 		Block mBlock;
+		//how big is a block? one ms?
+		mBlock.Cycles(1337);
 		Chunk mChunk;
+		//Sloppy but works for this case
+		mChunk.SizeWord((atoi(toConvert.bitsPerSample) * atoi(toConvert.numStreams)) / 8);
+		mChunk.CountWords(1);
+
+		Lump mLump;
+		
+		//now the fun part: adding streams.
+		for(std::vector<XStream * >::iterator stritr = toConvert.streams->begin(); stritr != toConvert.streams->end(); ++stritr) 
+		{
+			Stream * mStream = new Stream();
+
+			mStream->Id((*stritr)->name);
+
+			mStream->Packedbits(atoi(toConvert.bitsPerSample));
+			mStream->Quantization(atoi(toConvert.bitsPerSample));
+			
+			Band * bs = new Band( ((*stritr)->index) );
+			bs->CenterFrequency(*( new Frequency ( atof((*stritr)->carrier) , Frequency::Hz )) );
+			bs->TranslatedFrequency(*(new Frequency ( atof((*stritr)->intermediate) , Frequency::Hz) ) );
+
+			mStream->Bands().push_back(*bs);
+			mLump.Streams().push_back(*mStream);
+			
+		}
+		
 
 		//construct the hierarchy.
-		
+		mChunk.Lumps().push_back(mLump);		
 		mBlock.Chunks().push_back(mChunk);
 		mLane.Blocks().push_back(mBlock);
 		mLane.Systems().push_back(mSystem);
 		mFile.Lane(mLane);
 		md.Files().push_back(mFile);
 
-		/**
-		LUMP
-One or more lumps (specified in order) present in this chunk.
-Lump
-Required
-SIZEWORD
-Bytes of unsigned integer datatype that data shall be read as
-UINT8
-1, 2, 4, 8 (Corresponds to UINT8, UINT16, UINT32 and UINT64)
-Required
-COUNTWORDS
-Total number of words to be read in order to read/decode this chunk
-UINT8
-Required
-ENDIAN
-Endianness of words stored in chunk
-Endian
-‘L’ – Little
-‘B’ – Big
-‘N’ – not applicable
-yes
-‘N’
-PADDING
-Padding applied during encoding
-Padding
-‘H’ – head padding
-‘T’ – Tail padding
-‘N’ – No padding
-yes
-‘N’
-SHIFT
-Word shift direction
-WordShift
-‘L’ – Left shift
-‘R’ – Right shift
-Required
-		
-		*/
-		
+		cin.get();cin.get();
 
 		std::string * SDRXName = changeExt(XFile,"sdrx");
 		try
@@ -298,6 +180,7 @@ bool pullXMetadata(tinyxml2::XMLDocument * doc)
 	toConvert.footerMask = titleElement->FirstChildElement("FOOTERMASK")->GetText();
 	toConvert.sampleRateHz = titleElement->FirstChildElement("SAMPLERATEHZ")->GetText();
 	toConvert.nextFile = titleElement->FirstChildElement("NEXTFILENAME")->GetText();
+	toConvert.streams = new vector <XStream * > ();
 
 	int streamCount = atoi(toConvert.numStreams);
 
@@ -313,6 +196,7 @@ bool pullXMetadata(tinyxml2::XMLDocument * doc)
 		stream->carrier = streamNode->Attribute("FCAR");
 		stream->intermediate = streamNode->Attribute("FIF");
 
+		toConvert.streams->push_back(stream);
 		streamNode = streamNode->NextSibling()->ToElement();
 	
 	}
@@ -335,12 +219,3 @@ std::string * changeExt(std::string * XFilePtr, const char * ext)
 	retString->append(ext);
 	return retString;
 }
-
-/**
-First tgx file name
-PathsCsv (where to find the files, paths separated by commas – similar to the decoder)
-Splice Mode (int_32): 0: convert this file only, >0: attempt to find this many concurrent files and convert, <0: keep converting files until concurrent sequence ends
-Write Mode (bool): 0: write converted files to source dir and rename old file.tgx file to file.tgx_old; 1: write all converted files to app home directory
-Output log file of converter activity (session report in text format).
-NOTE: care should be taken to ‘do no harm’ to existing files. This app will be used to convert hundreds of thousands of files, some of which are the only copies we have.
-*/
